353165b32a780acabb648696b6455a35918714f8
aman0990 <raj.aman.726@gmail.com>
2023-12-19 09:03:57 +0530

    Taking Java basic to advance

---
+++ src/Loops/for_each_method.java
@@ -0,0 +1,17 @@
+package Loops;
+import java.util.Arrays;
+import java.util.function.Consumer;
+public class for_each_method {
+    public static void main(String[] args) {
+        var nums = Arrays.asList(10,20,30,40,50,60);
+       /* var con = new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) {
+                System.out.println(integer);
+            }
+        };
+        nums.forEach(con);*/
+        Consumer<Integer> con = integer -> System.out.println(integer);
+        nums.forEach(con);
+    }
+}
