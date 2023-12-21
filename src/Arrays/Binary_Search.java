69b1c62847b3f6627355e83fe9a60593e19f33e4
aman0990 <raj.aman.726@gmail.com>
2023-12-22 00:11:45 +0530

    Taking Java basic to advance

---
+++ src/Arrays/Binary_Search.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+public class Binary_Search {
+    public static void main(String[] args) {
+        int [] nums = {2,4,5,6,7,8,9};
+        int tgt = 9;
+
+        int result = BinarySearch(nums, tgt);
+        if (result != -1)
+            System.out.println("Element found at index:- "+result);
+        else System.out.println("Element not found at any index. ");
+    }
+    private static int BinarySearch(int[] nums, int tgt) {
+        int left = 0;
+        int right = nums.length-1;
+        while (left <= right){
+            int mid = (left + right)/2;
+            if(nums[mid] == tgt){
+                return mid;
+            } else if (nums[mid] < tgt) {
+                left = mid + 1;
+            }else right = mid - 1;
+        }
+        return -1;
+    }
+}
---
+++ src/Arrays/Linear_Search.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class Linear_Search {
+    public static void main(String[] args) {
+        int [] nums = {4,5,6,2,9,12,69};
+        int tgt = 2;
+
+        int result = linearSearch(nums, tgt);
+        if (result != -1)
+        System.out.println("Element found at index:- "+result);
+        else System.out.println("Element not found at any index. ");
+    }
+
+    private static int linearSearch(int[] nums, int tgt) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == tgt)
+                return i;
+        }
+        return -1;
+    }
+}
