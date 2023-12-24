7b33bfebac95bc700159f227479e930277a6375b
aman0990 <raj.aman.726@gmail.com>
2023-12-24 13:06:11 +0530

    Taking Java basic to advance

--- src/Arrays/Array_Suffle_X1_Y1.java
+++ src/Arrays/Array_Suffle_X1_Y1.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Array_Suffle_X1_Y1 {
     //input:- x1,x2,x3,y1,y2,y3 = output:- x1,y1,x2,y2,x3,y3
     public static int[] shuffle(int[] nums, int n) {
-        int result[] = new int[nums.length];
+        int[] result = new int[nums.length];
         int i = 0,j = n, c = 0;
         while(j < 2*n){
             result[c++] = nums[i++];
